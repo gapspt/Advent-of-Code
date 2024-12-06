d85c90e606503501a229849fab18a42e7897fdc0
gaps <gaps@no.email>
2024-12-06 12:56:54 +0000

    2024 - Day 4 - Part 2

--- 2024/src/day4.zig
+++ 2024/src/day4.zig
@@ -7,6 +7,7 @@ const ArrayList = std.ArrayList;
 const allocator = std.heap.page_allocator;
 
 var list = ArrayList([]const u8).init(allocator);
+var listRead = false;
 
 pub fn part1() !void {
     try readList();
@@ -52,15 +53,43 @@ pub fn part1() !void {
     try out.print("{}\n", .{sum});
 }
 
-pub fn part2() !void {}
+pub fn part2() !void {
+    try readList();
+    const items = list.items;
+
+    var sum: i64 = 0;
+
+    var r: i32 = 2;
+    const rows = items.len;
+    while (r < rows) : (r += 1) {
+        const ur: usize = @intCast(r);
+        const columns = items[ur].len;
+        var c: i32 = 2;
+        while (c < columns) : (c += 1) {
+            if ((checkMas(items, r - 2, c - 2, 1, 1) or checkMas(items, r, c, -1, -1)) and
+                (checkMas(items, r - 2, c, 1, -1) or checkMas(items, r, c - 2, -1, 1)))
+            {
+                sum += 1;
+            }
+        }
+    }
+
+    const out = io.getStdOut().writer();
+    try out.print("{}\n", .{sum});
+}
 
 fn readList() !void {
+    if (listRead) {
+        return;
+    }
+
     const input: []const u8 = @embedFile("input/day4.txt");
 
     var linesSplit = mem.splitScalar(u8, input, '\n');
     while (linesSplit.next()) |line| {
         try list.append(line);
     }
+    listRead = true;
 }
 
 fn checkXmas(items: [][]const u8, r: i32, c: i32, rInc: i32, cInc: i32) bool {
@@ -88,4 +117,24 @@ fn checkXmas(items: [][]const u8, r: i32, c: i32, rInc: i32, cInc: i32) bool {
         items[ur1][uc1] == 'M' and
         items[ur2][uc2] == 'A' and
         items[ur3][uc3] == 'S';
+}
+
+fn checkMas(items: [][]const u8, r: i32, c: i32, rInc: i32, cInc: i32) bool {
+    const rLimit: i32 = r + (rInc * 2);
+    const ur0: usize = @intCast(r);
+    const ur1: usize = @intCast(r + rInc);
+    const ur2: usize = @intCast(rLimit);
+
+    const cLimit: i32 = c + (cInc * 2);
+    const uc0: usize = @intCast(c);
+    const uc1: usize = @intCast(c + cInc);
+    const uc2: usize = @intCast(cLimit);
+
+    if (uc0 >= items[ur0].len or uc1 >= items[ur1].len or uc2 >= items[ur2].len) {
+        return false;
+    }
+
+    return items[ur0][uc0] == 'M' and
+        items[ur1][uc1] == 'A' and
+        items[ur2][uc2] == 'S';
 }
