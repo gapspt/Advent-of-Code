2a0037e422af7e60a4a8de5073b058536defd818
gaps <gaps@no.email>
2024-12-12 20:08:23 +0200

    2024 - Day 11 - Part 2

--- 2024/src/day11.zig
+++ 2024/src/day11.zig
@@ -2,16 +2,27 @@ const std = @import("std");
 const fmt = std.fmt;
 const io = std.io;
 const mem = std.mem;
-const DoublyLinkedList = std.DoublyLinkedList;
+const AutoHashMap = std.AutoHashMap;
 
-const ListNode = std.DoublyLinkedList(u64).Node;
-
-const allocator = std.heap.page_allocator;
+var allocator: mem.Allocator = std.heap.page_allocator;
 
 const input: []const u8 = @embedFile("input/day11.txt");
 
+var map: AutoHashMap(u64, []u64) = undefined;
+
 pub fn part1() !void {
-    var list = DoublyLinkedList(u64){};
+    try countStones(25);
+}
+
+pub fn part2() !void {
+    try countStones(75);
+}
+
+fn countStones(times: u32) !void {
+    map = AutoHashMap(u64, []u64).init(allocator);
+    defer map.deinit();
+
+    var sum: u64 = 0;
 
     var valuesSplit = mem.splitAny(u8, input, " \r\n");
     while (valuesSplit.next()) |valueStr| {
@@ -19,72 +30,77 @@ pub fn part1() !void {
             break;
         }
 
-        const node = try allocator.create(ListNode);
-        node.data = try fmt.parseInt(u64, valueStr, 10);
-        list.append(node);
-    }
-
-    var n: i32 = 25;
-    while (n > 0) : (n -= 1) {
-        var next = list.first;
-        while (next) |node| {
-            next = node.next;
-
-            var value = node.data;
-
-            // - If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
-            // - If the stone is engraved with a number that has an even number of digits, it is replaced by two stones.
-            //   The left half of the digits are engraved on the new left stone, and the right half of the
-            //   digits are engraved on the new right stone.
-            //   (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
-            // - If none of the other rules apply, the stone is replaced by a new stone;
-            //   the old stone's number multiplied by 2024 is engraved on the new stone.
-
-            if (value == 0) {
-                node.data = 1;
-            } else {
-                var digits: u32 = 1;
-                var v = value;
-                while (v > 9) : (v = @divTrunc(v, 10)) {
-                    digits += 1;
-                }
-
-                if (digits % 2 == 0) {
-                    const newNode = try allocator.create(ListNode);
-                    list.insertAfter(node, newNode);
-
-                    node.data = 0;
-                    newNode.data = 0;
-
-                    const halfDigits = digits / 2;
-                    var mult: u64 = 1;
-                    var i: u32 = 0;
-                    while (i < halfDigits) : (i += 1) {
-                        newNode.data += (value % 10) * mult;
-                        value = @divTrunc(value, 10);
-                        mult *= 10;
-                    }
-                    mult = 1;
-                    while (i < digits) : (i += 1) {
-                        node.data += (value % 10) * mult;
-                        value = @divTrunc(value, 10);
-                        mult *= 10;
-                    }
-                } else {
-                    node.data = value * 2024;
-                }
-            }
-        }
-    }
-
-    const sum = list.len;
-
-    while (list.pop()) |node| {
-        allocator.destroy(node);
+        const value = try fmt.parseInt(u64, valueStr, 10);
+        sum += try countStone(value, times);
     }
 
     const out = io.getStdOut().writer();
     try out.print("{}\n", .{sum});
 }
 
-pub fn part2() !void {}
+fn countStone(value: u64, times: u32) !u64 {
+    if (times == 0) {
+        return 1;
+    }
+
+    const getOrPut = try map.getOrPut(value);
+    if (!getOrPut.found_existing) {
+        getOrPut.value_ptr.* = try allocator.alloc(u64, times);
+        @memset(getOrPut.value_ptr.*, 0);
+    }
+    var arr = getOrPut.value_ptr.*;
+    if (arr.len < times) {
+        const len = arr.len;
+        arr = try allocator.alloc(u64, times);
+        @memcpy(arr[0..len], getOrPut.value_ptr.*[0..len]);
+        @memset(arr[len..], 0);
+        allocator.free(getOrPut.value_ptr.*);
+        getOrPut.value_ptr.* = arr;
+    }
+
+    const newTimes = times - 1;
+    if (arr[newTimes] != 0) {
+        return arr[newTimes];
+    }
+
+    var count: u64 = undefined;
+
+    // - If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
+    // - If the stone is engraved with a number that has an even number of digits, it is replaced by two stones.
+    //   The left half of the digits are engraved on the new left stone, and the right half of the
+    //   digits are engraved on the new right stone.
+    //   (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
+    // - If none of the other rules apply, the stone is replaced by a new stone;
+    //   the old stone's number multiplied by 2024 is engraved on the new stone.
+
+    if (value == 0) {
+        count = try countStone(1, newTimes);
+    } else {
+        var digits: u32 = 1;
+        var v = value;
+        while (v > 9) : (v = @divTrunc(v, 10)) {
+            digits += 1;
+        }
+
+        if (digits % 2 == 0) {
+            var value1: u64 = value;
+            var value2: u64 = 0;
+
+            const halfDigits = digits / 2;
+            var mult: u64 = 1;
+            var i: u32 = 0;
+            while (i < halfDigits) : (i += 1) {
+                value2 += (value1 % 10) * mult;
+                value1 /= 10;
+                mult *= 10;
+            }
+
+            count = try countStone(value1, newTimes) + try countStone(value2, newTimes);
+        } else {
+            count = try countStone(value * 2024, newTimes);
+        }
+    }
+
+    arr[newTimes] = count;
+    return arr[newTimes];
+}
