0e1dc0e4f0f2036eef137923a93f03598de225a3
gaps <gaps@no.email>
2024-12-17 12:01:58 +0200

    2024 - Day 16 - Part 2

--- 2024/src/day16.zig
+++ 2024/src/day16.zig
@@ -4,6 +4,7 @@ const math = std.math;
 const mem = std.mem;
 const Order = math.Order;
 const ArrayList = std.ArrayList;
+const AutoHashMap = std.AutoHashMap;
 const PriorityQueue = std.PriorityQueue;
 
 const Direction = enum { north, south, east, west };
@@ -15,6 +16,14 @@ var allocator: mem.Allocator = std.heap.page_allocator;
 const input: []const u8 = @embedFile("input/day16.txt");
 
 pub fn part1() !void {
+    try findBestPath(false);
+}
+
+pub fn part2() !void {
+    try findBestPath(true);
+}
+
+fn findBestPath(findAllBestLocations: bool) !void {
     var list = ArrayList([]const u8).init(allocator);
     defer list.deinit();
 
@@ -43,13 +52,14 @@ pub fn part1() !void {
         }
     }
 
-    const score = try findBestCost(list.items, dirStart, xStart, yStart, xEnd, yEnd);
+    var score = try findBestCost(list.items, dirStart, xStart, yStart, xEnd, yEnd);
+    if (findAllBestLocations) {
+        score = try findBestLocations(list.items, dirStart, xStart, yStart, xEnd, yEnd, score);
+    }
 
     const out = io.getStdOut().writer();
     try out.print("{}\n", .{score});
 }
-
-pub fn part2() !void {}
 
 fn findBestCost(map: [][]const u8, dirStart: Direction, xStart: i32, yStart: i32, xEnd: i32, yEnd: i32) !i32 {
     const h: i32 = @intCast(map.len);
@@ -104,6 +114,106 @@ fn findBestCost(map: [][]const u8, dirStart: Direction, xStart: i32, yStart: i32
         }
     }
     return -1;
+}
+
+fn findBestLocations(
+    map: [][]const u8,
+    dirStart: Direction,
+    xStart: i32,
+    yStart: i32,
+    xEnd: i32,
+    yEnd: i32,
+    bestCost: i32,
+) !i32 {
+    const h: i32 = @intCast(map.len);
+    const w: i32 = @intCast(map[0].len);
+    const a: i32 = h * w;
+
+    const minCosts = try allocator.alloc(i32, @intCast(w * h * 4));
+    defer allocator.free(minCosts);
+    @memset(minCosts, -1);
+
+    var bestLocations = std.AutoHashMap(i32, bool).init(allocator);
+    defer bestLocations.deinit();
+
+    const node = .{ .x = xStart, .y = yStart, .dir = dirStart, .cost = 0, .heuristic = 0 };
+
+    _ = try findBestLocationsVisit(map, h, w, a, minCosts, &bestLocations, xEnd, yEnd, bestCost, node);
+
+    return @intCast(bestLocations.count());
+}
+
+fn findBestLocationsVisit(
+    map: [][]const u8,
+    h: i32,
+    w: i32,
+    a: i32,
+    minCosts: []i32,
+    bestLocations: *AutoHashMap(i32, bool),
+    xEnd: i32,
+    yEnd: i32,
+    bestCost: i32,
+    node: SearchNode,
+) !bool {
+    if (node.heuristic > bestCost) {
+        return false;
+    }
+
+    const minCostsIndex: usize = @intCast(@intFromEnum(node.dir) * a + node.x * h + node.y);
+    const minCost = minCosts[minCostsIndex];
+    if (node.cost < minCost or minCost < 0) {
+        minCosts[minCostsIndex] = node.cost;
+    } else if (node.cost > minCost) {
+        return false;
+    }
+
+    if (node.x == xEnd and node.y == yEnd) {
+        try bestLocations.put(node.x * h + node.y, true);
+        return true;
+    }
+
+    var found = false;
+
+    var x = node.x;
+    var y = node.y;
+    var dir1: Direction = Direction.north;
+    var dir2: Direction = Direction.south;
+    if (node.dir == Direction.north or node.dir == Direction.south) {
+        dir1 = Direction.east;
+        dir2 = Direction.west;
+    }
+    var cost = node.cost + 1000;
+    const heuristic1 = optimisticHeuristic(x, y, dir1, xEnd, yEnd);
+    const heuristic2 = optimisticHeuristic(x, y, dir2, xEnd, yEnd);
+    const child1: SearchNode = .{ .x = x, .y = y, .dir = dir1, .cost = cost, .heuristic = cost + heuristic1 };
+    const child2: SearchNode = .{ .x = x, .y = y, .dir = dir2, .cost = cost, .heuristic = cost + heuristic2 };
+    if (try findBestLocationsVisit(map, h, w, a, minCosts, bestLocations, xEnd, yEnd, bestCost, child1)) {
+        found = true;
+    }
+    if (try findBestLocationsVisit(map, h, w, a, minCosts, bestLocations, xEnd, yEnd, bestCost, child2)) {
+        found = true;
+    }
+
+    switch (node.dir) {
+        Direction.north => y += -1,
+        Direction.south => y += 1,
+        Direction.east => x += 1,
+        Direction.west => x += -1,
+    }
+    if (x >= 0 and x < w and y >= 0 and y < h and map[@intCast(y)][@intCast(x)] != '#') {
+        const heuristic3 = optimisticHeuristic(x, y, node.dir, xEnd, yEnd);
+        cost = node.cost + 1;
+        const child3: SearchNode = .{ .x = x, .y = y, .dir = node.dir, .cost = cost, .heuristic = cost + heuristic3 };
+        if (try findBestLocationsVisit(map, h, w, a, minCosts, bestLocations, xEnd, yEnd, bestCost, child3)) {
+            found = true;
+        }
+    }
+
+    if (found) {
+        try bestLocations.put(node.x * h + node.y, true);
+    }
+
+    return found;
 }
 
 fn lessThan(_: ?bool, a: SearchNode, b: SearchNode) Order {
